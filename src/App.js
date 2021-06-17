6e52bbc088a8de2ae04c11a668a70c9422110c59
jagadeeshwaran <46racer2507@gmail.com>
2021-06-17 19:29:15 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,146 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const list = [
+    {
+        gen:'Female.NL',
+        name:'Ms. Teatske Nijenhuis',
+        mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+    {
+      gen:'Female.NL',
+      name:'Ms. Teatske Nijenhuis',
+      mail:'marius.misje@example.com'
+    },
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className='top-card'>
+          <div className="profile">
+            <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" />
+          </div>
+          <div className='detials'>
+            <div className='name'>
+              Ms. Teatske Nijenhuis
+            </div>
+            <div className='add'>
+              <span className='dooron' >775</span>,heindertweg, born, zeeland, <span className='land'>Netherlands</span>, 18818 +6:00 - Almaty, <span className='underline'>Dhaka</span>, Colombo
+            </div>
+            <div className='gen'>
+              Female
+            </div>
+          </div>
+        </div>
+        <div className='members-detials'>
+          {
+            list.map((item, i) => (
+              <div className='detial-cards' key={i} >
+                <div className='map-gen'>
+                  {item.gen}
+                </div>
+                <div className='map-name'>
+                  {item.name}
+                </div>
+                <div className='map-mail'>
+                  {item.mail}
+                </div>
+              </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
